e082117339996d5953172b0469e190fac52af9ab
Marc Leitner <marc.leitner@htl-salzburg.ac.at>
2019-03-27 15:04:15 +0100

    update gitignore, script edit with text changes

---
+++ Assets/MyGame/Scripts/FirstScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FirstScript : MonoBehaviour {
+
+
+    public Text myText;
+    public TextMeshProUGUI myTmpText;
+
+
+	// Use this for initialization
+	void Start () {
+
+        myText.text = "MC";
+        myTmpText.text = "Hello tmp text";
+
+        Debug.Log("Hello World");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
